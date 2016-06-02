be89703c457e2ec989642086e89e4f43329e417a
rayashari <rayashari94@gmail.com>
2016-06-02 19:00:22 +0900

    rev01

---
+++ app/src/main/java/com/rayasha/diarynotes/Write.java
@@ -0,0 +1,18 @@
+package com.rayasha.diarynotes;
+/**
+ * Created by rayasha on 6/2/2016.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Write extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.write_story);
+
+
+
+
+    }
+}
